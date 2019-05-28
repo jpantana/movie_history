507897faeb086698ee023ca6eb82654c75155301
JPantana <joshpantana@me.com>
2019-05-28 16:47:15 -0500

    things are hiding based on login status

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -7,6 +7,7 @@ const authDiv = document.getElementById('auth');
 const moviesNavbar = document.getElementById('navbar-button-movies');
 const authNavbar = document.getElementById('navbar-button-auth');
 const logoutNavbar = document.getElementById('navbar-button-logout');
+const navbarBrand = document.getElementById('navBarBrand');
 
 const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
@@ -15,11 +16,13 @@ const checkLoginStatus = () => {
       moviesNavbar.classList.remove('hide');
       authNavbar.classList.add('hide');
       logoutNavbar.classList.remove('hide');
+      navbarBrand.classList.remove('hide');
     } else {
       authDiv.classList.remove('hide');
       moviesNavbar.classList.add('hide');
       authNavbar.classList.remove('hide');
       logoutNavbar.classList.add('hide');
+      navbarBrand.classList.add('hide');
     }
   });
 };
